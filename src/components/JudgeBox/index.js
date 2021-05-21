3cdb6e50f22fd3cf7d27c984c01886f723db0a0a
HappyVirgo <happy0910virgo@outlookcom>
2021-05-21 08:49:58 -0700

    add skating page

---
+++ src/components/JudgeBox/index.js
@@ -0,0 +1,73 @@
+import React from 'react';
+
+import './index.scss'
+
+const JudgeBox = ({data, idx, isMobile}) => {
+    const [show, setShow] = React.useState(false);
+    const showDetails = () => {
+        setShow(!show);
+    }
+    return (
+        <div>
+            <div className="gap-2-c flex items-center pl-3">
+                <span className={`tag uppercase ${!isMobile&&'desktop'}`}>Current ARP</span>
+                <span className={`arp ${!isMobile&&'desktop'}`}>{data.curArp}$</span>
+            </div>
+            <div className={`skating-card flex flex-col items-center ${!isMobile&&'desktop'}`}>
+                <div className="flex usdt-wrapper pt-3">
+                    <span className="white-tag">USDT_JUDGE</span>
+                    <div className="flex tvl-wrapper">
+                        <span className="white-tag">TVL</span>
+                        <span className="tvl">{data.tvl}$</span>
+                    </div>
+                </div>
+                <div className="flex gap-6">
+                    <div className={`box stacked-box flex flex-col gap-1 ${!isMobile&&'desktop'}`}>
+                        <span className="title">Staked USD</span>
+                        <span className="amount">${data.stacked}</span>
+                        <span className={`description ${!isMobile&&'desktop'}`}>({data.stacked} LP Token)</span>
+                    </div>
+                    <div className={`box earned-box flex flex-col gap-1 ${!isMobile&&'desktop'}`}>
+                        <span className="title">JUDGE Earned</span>
+                        <span className="amount">{data.earned}</span>
+                        <span className={`description ${!isMobile&&'desktop'}`}>(${data.earned})</span>
+                    </div>
+                </div>
+                <div className="flex flex-col">
+                    <div className="flex items-center justify-between">
+                        <span className={`description ${!isMobile&&'desktop'}`}>wallet:{data.wallet}</span>
+                        <span className={`badge ${!isMobile&&'desktop'}`}>max</span>
+                    </div>
+                    <input type="text" placeholder="Enter amount" className="amount-input p-1 text-center" />
+                    <button className={`connect-button pt-1 ${!isMobile&&'desktop'}`}>connect wallet</button>
+                    <span className="details-text pb-3 text-center items-center" onClick={showDetails}>
+                        Details
+                        <svg className="box__arrow inline ml-2" style={{transform: show&&'rotate(180deg)'}} width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M13.5 5L8 10.5L2.5 5" stroke="white" stroke-width="1.5"></path>
+                        </svg>
+                    </span>
+                    {show&&<div className="flex flex-col">
+                        <div className={`detail flex justify-between items-center ${!isMobile&&'desktop'}`}>
+                            <span className="details-tag">Staked</span>
+                            <span className="amount">0 LP tokens ($0)</span>
+                        </div>
+                        <div className={`detail flex justify-between items-center ${!isMobile&&'desktop'}`}>
+                            <span className="details-tag">XFIT earned</span>
+                            <span className="amount">0 JUDGE ($0)</span>
+                        </div>
+                        <div className={`detail flex justify-between items-center ${!isMobile&&'desktop'}`}>
+                            <span className="details-tag">Rewards harvested</span>
+                            <span className="amount">0 JUDGE ($0)</span>
+                        </div>
+                        <div className={`detail flex justify-between items-center ${!isMobile&&'desktop'}`}>
+                            <span className="details-tag">Pool share</span>
+                            <span className="amount">0%</span>
+                        </div>
+                    </div>}
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default JudgeBox;
--- src/pages/SkatingPool/index.js
+++ src/pages/SkatingPool/index.js
@@ -1,8 +1,57 @@
 import React from 'react'
+import {useMediaQuery} from 'react-responsive'
+
+import JudgeBox from '../../components/JudgeBox'
+import Header from '../../components/Header'
+
+import './index.scss'
+
+const judges = [
+    {
+        curArp: 150,
+        tvl: 120,
+        stacked: 0,
+        earned: 0,
+        wallet: 100
+    },
+    {
+        curArp: 150,
+        tvl: 120,
+        stacked: 0,
+        earned: 0,
+        wallet: 100
+    }
+]
 
 const SkatingPool = () => {
+    const isSMDesktop = useMediaQuery({ query: `(max-width: 1420px)` });
+    const isMobile = useMediaQuery({ query: `(max-width: 768px)` });
+    const isXSMobile = useMediaQuery({ query: `(max-width: 390px)` });
     return (
-        <div>Skating Pool</div>
+        <div className="flex flex-col items-center justify-center">
+            <div className={`flex items-center justify-center headerWrapper ${!isMobile&&'desktop'}`}>
+                <Header text="Skating Pool" className="bottom-bordered mt-3" color="#FAFAFA" isMobile={isMobile} />
+            </div>
+            <div className={`judges flex flex-wrap justify-center align-items-center ${!isMobile&&'desktop'}`}>
+                {judges.map((judge, idx) => {
+                    return (
+                        <JudgeBox key={idx} idx={idx} data={judge} isMobile={isMobile} />
+                    )
+                })}
+            </div>
+            <div className="flex flex-col">
+                <div className={`how-work flex flex-col items-center ${!isMobile&&'desktop'}`}>
+                    <div className={`headerWrapper ${!isMobile&&'desktop'} text-center`}>
+                        <Header text="How does Judgement staking platform work?" className="paddingTop50 text-center" isMobile={isMobile} />
+                    </div>
+                    <span className="description">There are two ways to stake on our platform.</span>
+                </div>
+                <div className={`how-work-description flex flex-col ${!isMobile&&'desktop'}`}>
+                    <span className="description"><span className="first-text">1</span>Stake our tradable token <span className="tag">$JUDGE</span>. Whenever you stake $JUDGE, you will earn <span className="tag">$REDEEM</span> tokens. $REDEEM tokens can be redeemed for our NFTs in the NFT Redemption section of our App.</span>
+                    <span className="description"><span className="first-text">1</span>Stake our tradable token <span className="tag">$JUDGE</span>. Whenever you stake $JUDGE, you will earn <span className="tag">$REDEEM</span> tokens. $REDEEM tokens can be redeemed for our NFTs in the NFT Redemption section of our App.</span>
+                </div>
+            </div>
+        </div>
     )
 }
 
